1e9792be76faa19de5623ee3bc11f0d2c8d1b8cb
Arpita Ranawat <btbtc25116_arpita@banasthali.in>
2026-09-04 00:09:12 +0530

    Update index.ts

--- artifacts/api-server/api/index.ts
+++ artifacts/api-server/api/index.ts
@@ -1,3 +1,9 @@
 import app from "../src/app.js";
 
-export default app;
+export default function handler(req: any, res: any) {
+  if (!req.url.startsWith("/api")) {
+    req.url = `/api${req.url}`;
+  }
+
+  return app(req, res);
+}
